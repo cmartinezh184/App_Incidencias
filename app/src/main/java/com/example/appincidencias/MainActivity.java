2316fdf5891e4695c7f19d190ea9fdece745f6e6
Cristhian Martinez <cmartinezh2623@hotmail.com>
2019-10-18 17:29:40 -0600

    Pantalla de inicio de sesión. 5:29 PM 18/10/19.

--- app/src/main/java/com/example/appincidencias/MainActivity.java
+++ app/src/main/java/com/example/appincidencias/MainActivity.java
@@ -13,18 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.this.setTitle("Bienvenido!");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Button btn = (Button)findViewById(R.id.button4);
+        // Creacion de los botones para el cambio de pantalla
+        Button registrar = (Button)findViewById(R.id.button4);
+        Button login = (Button)findViewById(R.id.button3);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        // Redireccionamiento de los botones a las actividades correspondiedtes
+
+            // Registro de Usuario
+        registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrarUsuarioActivity.class));
             }
         });
+
+            // Inicio de Sesion
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
+
 }
