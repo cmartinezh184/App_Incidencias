7e241fa0655f3a4bc575254c9c5bb8531c220016
Katherine Martínez <36804849+kathymch@users.noreply.github.com>
2019-10-18 22:30:10 -0600

    cambios menu principal

---
+++ app/src/main/java/com/example/appincidencias/menuPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.appincidencias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
